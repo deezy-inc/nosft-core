0682f76853e92015a87a9c5ab1ce407262711eec
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-06-29 21:49:54 -0600

    1.2.10

--- src/app/wallet.ts
+++ src/app/wallet.ts
@@ -84,6 +84,7 @@ const Wallet = function (config) {
             };
 
             await getAddress(getAddressOptions);
+            console.log('[xverse]', { ordinalsPublicKey, ordinalsAddress, paymentAddress });
             return { ordinalsPublicKey, ordinalsAddress, paymentAddress };
         },
         connectWallet: async (provider) => {
